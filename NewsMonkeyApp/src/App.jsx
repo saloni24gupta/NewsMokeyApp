1597764aecdeadf116342ea2cb955caf662edbe4
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-05-19 18:09:08 +0530

    files

---
+++ NewsMonkeyApp/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import News from './components/News'
+
+function App() {
+return (
+    <>
+     <div>
+      <Navbar />
+      <News />
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ NewsMonkeyApp/src/components/NewsItem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        tgis is newsitem
+      </div>
+    )
+  }
+}
+
+export default NewsItem

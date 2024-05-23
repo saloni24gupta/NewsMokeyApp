35c1fed648dec723e178ceaeeb4936840d5e0d00
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-05-23 07:59:43 +0530

    push

--- NewsMonkeyApp/src/components/NewsItem.jsx
+++ NewsMonkeyApp/src/components/NewsItem.jsx
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..."   />
+          <img src={!imageUrl ?  "https://readwrite.com/wp-content/uploads/2024/04/43315ab7-69d3-46cd-9d1a-12a0569aeebe-900x514.webp" : imageUrl } className="card-img-top" alt="..."   />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank"  className="btn btn-sm btn-primary">Read More</a>
+            <a href={newsUrl} target="_blank"  className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </div>
